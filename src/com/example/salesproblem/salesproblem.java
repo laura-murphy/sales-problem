56d2428cfca8ebc225641a07f45c1e05ee2bf21a
Laura Murphy <me@lauramurphy.name>
2017-03-25 12:09:34 +0000

    Initial commit

---
+++ src/com/example/salesproblem/salesproblem.java
@@ -0,0 +1,10 @@
+package com.example.salesproblem;
+
+/**
+ * Created by Laura on 25/03/2017.
+ */
+public class salesproblem {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
